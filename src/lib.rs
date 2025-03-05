02a43e575f97e1b334facaf3a5c690ac296d008e
Lukas Rieger <code@lukasrieger.com>
2025-03-05 03:54:03 +0100

    better thread handling, version 0.0.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,5 @@
-use std::future::Future;
-
-use async_std::channel::*;
-
-#[cfg(test)] use async_std::task;
-
-
 pub struct ReplyChannel<T> {
-    s: Sender<T>,
+    s: async_std::channel::Sender<T>,
 }
 
 impl<T> ReplyChannel<T> {
@@ -16,21 +9,18 @@ impl<T> ReplyChannel<T> {
     }
 }
 
-
 pub struct MailBox<TMessage, THandle> {
-    sender: Sender<TMessage>,
-    pub handle: THandle
+    sender: async_std::channel::Sender<TMessage>,
+    pub handle: THandle,
 }
-
 
 impl<TMessage, THandle> MailBox<TMessage, THandle> {
     pub fn post(&self, msg: TMessage) {
         self.sender.send_blocking(msg).unwrap();
     }
 
-    pub async fn ask<TResult>(&self, cb: fn(ReplyChannel<TResult>) -> TMessage) -> TResult{
-
-        let (s,r) = bounded(1);
+    pub async fn ask<TResult>(&self, cb: fn(ReplyChannel<TResult>) -> TMessage) -> TResult {
+        let (s, r) = async_std::channel::bounded(1);
 
         let rc = ReplyChannel { s };
         let msg = cb(rc);
@@ -39,9 +29,8 @@ impl<TMessage, THandle> MailBox<TMessage, THandle> {
     }
 }
 
-
 pub struct MailboxContext<TMessage> {
-    receiver: Receiver<TMessage>
+    receiver: async_std::channel::Receiver<TMessage>,
 }
 
 impl<TMessage> MailboxContext<TMessage> {
@@ -52,37 +41,89 @@ impl<TMessage> MailboxContext<TMessage> {
 
 pub enum MailboxBounds {
     Unbounded,
-    Bounded(usize)
+    Bounded(usize),
 }
 
-pub fn start_mailbox<TMessage, F, Fut, THandle>(bounds: MailboxBounds, f: F, spawn: fn(Fut) -> THandle) -> MailBox<TMessage, THandle> 
+/// starts a mailbox, where the caller needs to handle actually starting the async fn.
+/// 
+/// ```
+/// # use mailboxxy::*;
+/// # enum TestMsg { }
+/// # async fn my_mailbox_fn(ctx: MailboxContext<TestMsg>) { }
+/// start_mailbox_direct(MailboxBounds::Unbounded, |ctx| async_std::task::spawn(my_mailbox_fn(ctx)));
+/// ```
+pub fn start_mailbox_direct<TMessage, F, THandle>(
+    bounds: MailboxBounds,
+    f: F,
+) -> MailBox<TMessage, THandle>
 where
-    F : FnOnce(MailboxContext<TMessage>) -> Fut,
-    Fut : Future<Output = ()>
+    F: FnOnce(MailboxContext<TMessage>) -> THandle,
 {
-    let (s,r) = match bounds {
-        MailboxBounds::Unbounded => unbounded(),
-        MailboxBounds::Bounded(n) => bounded(n)
+    let (s, r) = match bounds {
+        MailboxBounds::Unbounded => async_std::channel::unbounded(),
+        MailboxBounds::Bounded(n) => async_std::channel::bounded(n),
     };
 
     let ctx = MailboxContext { receiver: r };
 
-    let handle = spawn(f(ctx));
+    let handle = f(ctx);
 
-    return MailBox { sender: s, handle };
+    MailBox { sender: s, handle }
 }
 
+/// starts a mailbox using a given executor. Examples can include 'async_std::task::spawn' or 'std::thread::spawn'.
+pub fn start_mailbox<TMessage, F, Fut, THandle>(
+    bounds: MailboxBounds,
+    f: F,
+    spawn: fn(Fut) -> THandle,
+) -> MailBox<TMessage, THandle>
+where
+    F: FnOnce(MailboxContext<TMessage>) -> Fut,
+{
+    start_mailbox_direct(bounds, |ctx| spawn(f(ctx)))
+}
+
+#[cfg(feature = "thread")]
+pub fn start_mailbox_on_thread<TMessage, F, Fut>(
+    bounds: MailboxBounds,
+    f: F,
+) -> MailBox<TMessage, std::thread::JoinHandle<()>>
+where
+    F: std::marker::Send + 'static + FnOnce(MailboxContext<TMessage>) -> Fut,
+    Fut: std::future::Future<Output = ()>,
+    TMessage: std::marker::Send + 'static,
+{
+    start_mailbox_direct(bounds, |ctx| {
+        std::thread::spawn(move || {
+            let fut = f(ctx);
+            async_std::task::block_on(fut)
+        })
+    })
+}
+
+pub fn start_mailbox_as_task<TMessage, F, Fut>(
+    bounds: MailboxBounds,
+    f: F,
+) -> MailBox<TMessage, async_std::task::JoinHandle<()>>
+where
+    F: std::marker::Send + 'static + FnOnce(MailboxContext<TMessage>) -> Fut,
+    Fut: std::future::Future<Output = ()> + std::marker::Send + 'static,
+    TMessage: std::marker::Send + 'static,
+{
+    start_mailbox_direct(bounds, |ctx| async_std::task::spawn(f(ctx)))
+}
+
+// ------------------------------------------------------------------------
 
 #[cfg(test)]
 enum TestMsg {
     Increment,
     Decrement,
-    GetValue(ReplyChannel<i32>)
+    GetValue(ReplyChannel<i32>),
 }
 
 #[cfg(test)]
-async fn mailbox_fn(ctx:MailboxContext<TestMsg>) {
-
+async fn mailbox_fn(ctx: MailboxContext<TestMsg>) {
     // local state
     let mut count = 0;
 
@@ -90,55 +131,92 @@ async fn mailbox_fn(ctx:MailboxContext<TestMsg>) {
         let msg: TestMsg = ctx.dequeue().await;
 
         match msg {
-            TestMsg::Increment => count = count + 1,
-            TestMsg::Decrement => count = count - 1,
-            TestMsg::GetValue(rc) => rc.reply(count)
+            TestMsg::Increment => count += 1,
+            TestMsg::Decrement => count -= 1,
+            TestMsg::GetValue(rc) => rc.reply(count),
         }
     }
 }
 
 #[cfg(test)]
-async fn test_async() {
-
-    let mb = start_mailbox(MailboxBounds::Unbounded, mailbox_fn, task::spawn);
-
+async fn test_mb<T>(mb: &MailBox<TestMsg, T>) {
     mb.post(TestMsg::Increment);
     mb.post(TestMsg::Increment);
     mb.post(TestMsg::Increment);
-    let val = mb.ask(|rc| TestMsg::GetValue(rc)).await;
+    let val = mb.ask(TestMsg::GetValue).await;
     assert_eq!(val, 3);
 
     mb.post(TestMsg::Decrement);
-    let val = mb.ask(|rc| TestMsg::GetValue(rc)).await;
+    let val = mb.ask(TestMsg::GetValue).await;
     assert_eq!(val, 2);
 }
 
 #[cfg(test)]
-async fn test_thread() {
-    let mb = start_mailbox(MailboxBounds::Unbounded, mailbox_fn, |fut| {
-        std::thread::spawn(move || {
-            smol::block_on(fut) // Run the future inside the thread
-        })
-    });
-
-    mb.post(TestMsg::Increment);
-    mb.post(TestMsg::Increment);
-    mb.post(TestMsg::Increment);
-    let val = mb.ask(|rc| TestMsg::GetValue(rc)).await;
-    assert_eq!(val, 3);
-
-    mb.post(TestMsg::Decrement);
-    let val = mb.ask(|rc| TestMsg::GetValue(rc)).await;
-    assert_eq!(val, 2);
+async fn test_async() {
+    let mb = start_mailbox(MailboxBounds::Unbounded, mailbox_fn, async_std::task::spawn);
+    test_mb(&mb).await;
 }
-
 
 #[test]
 fn run_test_async() {
     smol::block_on(test_async());
 }
 
+// ----------
+
+#[cfg(test)]
+async fn test_thread() {
+    let mb = start_mailbox_on_thread(MailboxBounds::Unbounded, mailbox_fn);
+    test_mb(&mb).await;
+}
+
 #[test]
 fn run_test_thread() {
     smol::block_on(test_thread());
+}
+
+// ----------
+
+// This tests that a fn, which is NOT Send, can be excuted on the thread executor.
+// (It can't be executed as a task, since that would require Send'ing it between threads on resumption points.)
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::collections::HashMap;
+
+    trait TTest {
+        fn test(&self) -> i32;
+    }
+
+    async fn mailbox_fn(ctx: MailboxContext<TestMsg>) {
+        // local state
+        let mut count = 0;
+        let box_: HashMap<i32, Box<dyn TTest>> = HashMap::new();
+
+        loop {
+            let msg: TestMsg = ctx.dequeue().await;
+
+            match msg {
+                TestMsg::Increment => count += 1,
+                TestMsg::Decrement => count -= 1,
+                TestMsg::GetValue(rc) => {
+                    box_.get(&count);
+                    rc.reply(count)
+                }
+            }
+        }
+    }
+
+    #[cfg(test)]
+    async fn test_thread() {
+        let mb = start_mailbox_on_thread(MailboxBounds::Unbounded, mailbox_fn);
+        test_mb(&mb).await;
+    }
+
+    #[test]
+    fn run_test_thread() {
+        smol::block_on(test_thread());
+    }
 }
